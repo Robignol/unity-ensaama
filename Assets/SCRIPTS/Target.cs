b2f18988658a27aba18cb8314b305bde51a0db18
Robignol <monchorobin@gmail.com>
2021-09-30 12:33:07 +0200

    eye-cours-joseph

--- Assets/SCRIPTS/Target.cs
+++ Assets/SCRIPTS/Target.cs
@@ -2,16 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// eye target + 
 public class Target : MonoBehaviour
 {
     // Start is called before the first frame update
    public Transform target;
+   public bool isFalling = false;
+   public GameObject clone;
 
     // Update is called once per frame
     void Update()
     {
-        if (target !=null){
+        if (target != null){
+            if (isFalling == false) {
                 transform.LookAt(target);
+            }
         }
+    }
+
+    void OnMouseDown() {
+        GameObject clone = Instantiate(gameObject, transform.parent);
+        clone.SetActive(false);
+
+
+         isFalling = true;
+         Rigidbody body = gameObject.AddComponent<Rigidbody>();
+        //"trainée angulaire", càd la friction associée à la rotation.
+        //0?05 par défaut, nous mettons 0,2f pour ralentir les yeux.
+        body.angularDrag = 0.8f;
+
+        Debug.Log("i'm fallllliiiiiinnnngg");
+
+        StartCoroutine(FallEnd());
+
+
+
+    }
+
+//pénible mais méthode dans unity pour retarde une éxécution, un délai. > fonction type IEnumerator
+    IEnumerator FallEnd(){
+        yield return new WaitForSeconds(4f);
+        isFalling = false;
+
+        Debug.Log("im still alive youhoo");
+
+        clone.SetActive(true);
     }
 }
--- README.md
+++ README.md
@@ -19,3 +19,30 @@ GameObject clone = Instantiate(gameObject);
 Destroy(clone, 5f);
 ```
 [voir usage CloneOnClick.cs](./Assets/CloneOnClick.cs)
+
+## Component (accès à une instance, ajouter, détruire)
+
+Pour récup un composant existant sur un gameObject,
+on utilise `GetComponent<MonComposant>()` par exemple:
+```csharp
+Rigidbody body = GetComponent<Rigidbody>();
+```
+
+Attention, le composant récupéré peut être nul (s'il n'existe pas)
+
+pour ajouter un composant sur un gameObject, 
+on utilise `gameObject.AddComponent<MonComposant>(); par exemple : 
+```csharp
+gameObject.AddComponent<Rigidbody>();
+```
+voir usage dans Target (eyes)
+
+// aller voir README de Joseph.
+
+
+## Debug
+
+pour mettre pause : 
+```csharp
+Debug.Break();
+```
